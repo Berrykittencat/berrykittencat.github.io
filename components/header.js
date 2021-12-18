0f8eb3e37474c33db024620f9436a242bc8a5b36
BlindArcher <robbe.decraemer@ucll.be>
2021-12-18 18:49:46 +0100

    Overzicht

---
+++ components/header.js
@@ -0,0 +1,61 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+<header class="ui fluid segment">
+    <h2 class="ui left floated header">
+        <img class="ui image" src="images/logo.png">
+        <div class="content">Vakantiepark X</div>
+    </h2>
+    <div class="ui right floated tiny buttons">
+        <button class="ui positive button active">NL</button>
+        <div class="or"></div>
+        <button class="ui button">EN</button>
+    </div>
+    <img class="ui fluid rounded image" src="images/banner1_crop.png">
+    <nav class="ui horizontal bulleted link large list">
+        <a id="nav_0" class="item">Overzicht</a>
+        <a id="nav_1" class="item">Accomodatie</a>
+        <a id="nav_2" class="item">Faciliteiten</a>
+        <a id="nav_3" class="item">Activiteiten</a>
+        <a id="nav_4" class="item">Restaurants & Shops</a>
+        <a id="nav_5" class="item">Trips</a>
+        <a id="nav_6" class="item">Prijzen</a>
+        <a id="nav_7" class="item">Plattegrond</a>
+    </nav>
+</header>
+
+<style>
+    header {
+        padding: 0.5em 4em 1.5em !important;
+    }
+
+    header > h2 {
+        margin-bottom: 0.5em !important;
+    }
+
+    header > h2 > img {
+        width: 4em !important;
+        margin-right: -0.5em;
+    }
+
+    header > .tiny.buttons {
+        margin-top: 14px !important;
+    }
+</style>
+`;
+
+class NavHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+  
+    connectedCallback() {
+        const semantic = document.querySelector('link[href*="semantic"]');
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+        shadowRoot.appendChild(headerTemplate.content);
+        shadowRoot.getElementById("nav_" + this.getAttribute("page")).classList.add("active");
+        if (semantic) shadowRoot.appendChild(semantic.cloneNode());
+    }
+  }
+  
+  customElements.define('nav-header', NavHeader);
